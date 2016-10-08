a48e3fd5a69a9e7d4151ce287cc0679289ba31ec
David Vink <david.vink@hotmail.com>
2016-10-08 15:36:17 +0200

    Made the MenuManager and minor tweaks on animations.

---
+++ Sepi's Tiny Arcade/Assets/Scripts/Main/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+    Animator menuAnim;
+    GameObject currMenu;
+
+    void Start() {
+        currMenu = transform.FindChild("Menus").FindChild("MainMenu").gameObject;
+        menuAnim = currMenu.GetComponent<Animator>();
+        menuAnim.SetBool("IsOpen", true);
+    }
+
+    public void ChangeMenu (GameObject menu){
+        menuAnim.SetBool("IsOpen", false);
+        menuAnim = menu.GetComponent<Animator>();
+        menuAnim.SetBool("IsOpen", true);
+        currMenu = menu;
+    }
+}
