64718820473c4a3974c365ff0bfed9c4f64b13d5
David Vink <david.vink@hotmail.com>
2016-11-06 10:47:53 +0100

    Main Menu Functionlity work

--- Sepi's Tiny Arcade/Assets/Scripts/Main/MenuManager.cs
+++ Sepi's Tiny Arcade/Assets/Scripts/Main/MenuManager.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class MenuManager : MonoBehaviour {
     Animator menuAnim;
     GameObject currMenu;
+
+    public enum Events {Quit, Scene}
 
     void Start() {
         currMenu = transform.FindChild("Menus").FindChild("MainMenu").gameObject;
@@ -20,6 +23,17 @@ public class MenuManager : MonoBehaviour {
     public void Update() {
         if (Input.GetButtonDown("Cancel")) {
             ChangeMenu(transform.FindChild("Menus").FindChild("MainMenu").gameObject);
+        }
+    }
+    public void MenuEvent (Events EOption, int option) {
+        switch (){
+            case (Events)0  :
+                Application.Quit();
+                break;
+            case (Events)1 :
+                Application.LoadLevel(option);
+                break;  
+
         }
     }
 }
